4ee07e1861bdedf14b2b21fe55f641a178159221
isaac <aderonmuisaac@gmail.com>
2020-07-25 18:14:01 +0100

    feat: database setup

--- models.py
+++ models.py
@@ -1,1 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+
+# Path is stored in the virtual environment
+# with a key name of DATABASE_PATH 
+database_path = os.environ['DATABASE_PATH']
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def db_setup(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    migrate = Migrate(app, db)
