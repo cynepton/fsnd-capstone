3050f3da7a8512877c9d5aa59e30418d55bb77e1
isaac <aderonmuisaac@gmail.com>
2020-07-27 09:00:26 +0100

    chore: shell file with environment variables

--- setup.sh
+++ setup.sh
@@ -1,1 +1,18 @@
 
+# Database URI
+export DATABASE_PATH = postgresql://cynepton:cynepton@localhost:5432/fsnd-capstone
+
+# Environment Variables
+export ITEMS_PER_PAGE = 10
+
+# Flask App config
+export FLASK_APP = app
+export FLASK_ENV = development
+
+# Configurations gotten from the account created on Auth0
+export AUTH0_DOMAIN = 'udacity-fsnd-capstone.us.auth0.com'
+export ALGORITHMS = ['RS256']
+export API_AUDIENCE = 'casting_agency'
+
+export AUTH0_CLIENT_ID = "eDxU1gLQJog9fqRGBY7kR3dO7L23QZYB"
+export AUTH0_CALLBACK_URL = "https://localhost:5000/"
