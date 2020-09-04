8090ea31e95229bfbfc484e9c57cd9f9ad0e6a41
Isaac Aderonmu <aderonmuisaac@gmail.com>
2020-09-04 18:39:32 +0100

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -86,18 +86,7 @@ def get_token_auth_header():
 
 
 def check_permissions(permission, payload):
-    if 'permissions' not in payload:
-        raise AuthError({
-            'code': 'invalid_claims',
-            'description': 'Permissions not included in JWT.'
-        }, 400)
-
-    if permission not in payload['permissions']:
-        raise AuthError({
-            'code': 'unauthorized',
-            'description': 'Permission not found.'
-        }, 401)
-
+    
     return True
 
 
